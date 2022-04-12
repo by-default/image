90da8e2174e046fd509af2adc883d8af8727b926
aanper <mail@s3f.ru>
2022-04-12 15:23:14 +0300

    read testmode and dip values

--- ontop/boot/bydefault/gp.py
+++ ontop/boot/bydefault/gp.py
@@ -12,6 +12,12 @@ DIP = [19,20,26,21,6,12,13,16]
 for gpio in DIP:
     GPIO.setup(gpio, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# LEDs
+LED = [11, 9, 10]
+for led in LED:
+    GPIO.setup(led, GPIO.OUT)
+    GPIO.output(led, False)
+
 def read_gpio():
     state = {}
     state["test"] = bool(GPIO.input(TEST_MODE))
@@ -21,7 +27,12 @@ def read_gpio():
 
 try:
     while True:
-        print(read_gpio())
+        gpio_state = read_gpio()
+        print(gpio_state)
+        GPIO.output(LED[0], gpio_state["dip"][0])
+        GPIO.output(LED[1], gpio_state["dip"][1])
+        GPIO.output(LED[2], gpio_state["dip"][2])
+        
         time.sleep(0.5)
 
 except KeyboardInterrupt:
