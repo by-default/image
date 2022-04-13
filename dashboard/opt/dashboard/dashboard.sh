9186d7cac5fb46cf91d364c235bc192a29150f73
aanper <mail@s3f.ru>
2022-04-13 16:57:33 +0300

    move dashboard

---
+++ dashboard/opt/dashboard/dashboard.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+PIN=4
+
+trap ctrl_c INT
+function ctrl_c() {
+    echo "go away by default"
+    if [[ -d /sys/class/gpio/gpio$PIN ]]; then
+        echo $PIN >/sys/class/gpio/unexport
+    fi
+    kill $DASHBOARD_PID
+    reset
+    sudo chvt $LAST_TTY
+    exit 0
+}
+
+raspi-gpio set $PIN ip pu
+
+if [[ ! -d /sys/class/gpio/gpio$PIN ]]; then
+    echo $PIN >/sys/class/gpio/export
+fi
+
+while true; do
+    # wait for gpio pull
+    while [[ $(cat /sys/class/gpio/gpio$PIN/value) == "1" ]]; do sleep 1s; done
+
+    # save current tty
+    LAST_TTY=`sudo fgconsole`
+    MY_TTY=`tty | sed 's/\/dev\/tty//g'`
+
+    /usr/bin/sampler --config config.yml & DASHBOARD_PID=$!
+    sudo chvt $MY_TTY
+
+    # wait for gpio release
+    while [[ $(cat /sys/class/gpio/gpio$PIN/value) == "0" ]]; do sleep 1s; done
+    kill $DASHBOARD_PID
+    reset
+    sudo chvt $LAST_TTY
+done
